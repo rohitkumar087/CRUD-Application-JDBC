c2d79d92bb41a47605e579cfffbd0c957d0cf86f
rohit kumar <rohitkumarprajapat087@gmail.com>
2025-12-13 01:10:16 +0530

    add Student DTO for data transfer

---
+++ jdbcCRUDApp/src/in/javatech/dto/Student.java
@@ -0,0 +1,40 @@
+package in.javatech.dto;
+
+import java.io.Serializable;
+
+public class Student implements Serializable{
+	private Integer sid;
+	private String sname;
+	private String sage;
+	private String saddress;
+	public Integer getSid() {
+		return sid;
+	}
+	public void setSid(Integer sid) {
+		this.sid = sid;
+	}
+	public String getSname() {
+		return sname;
+	}
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+	public String getSage() {
+		return sage;
+	}
+	public void setSage(String sage) {
+		this.sage = sage;
+	}
+	public String getSaddress() {
+		return saddress;
+	}
+	public void setSaddress(String saddress) {
+		this.saddress = saddress;
+	}
+	
+	@Override
+	public String toString() {
+		return "Student [sid=" + sid + ", sname=" + sname + ", sage=" + sage + ", saddress=" + saddress + "]";
+	}
+	
+}
